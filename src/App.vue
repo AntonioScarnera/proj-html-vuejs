5bf557041408cc9e585e187bcc0c19bdb46a67ea
AntonioScarnera <scarnerantonio07@gmail.com>
2022-05-16 11:19:10 +0200

    scuffolding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <!-- Inizio Header -->
+    <header>
+
+      <!-- Aggiunta componente per la navabar -->
+      <navbar-comp/>
+
+      <!-- Creazione sezione per il Jumbotron -->
+      <section id="jumbo">
+
+        <div>
+          
+        </div>
+
+        <div>
+
+          <!-- Aggiunta componente per il gruppo di input -->
+          <inputs-form/>
+        </div>
+
+      </section>
+
+    </header>
+    <!-- Fine Header -->
+
+    <!-- Inizio Main -->
+    <main>
+
+      <!-- Creazione sezione 1 -->
+      <section id="sec-1">
+
+      </section>
+
+      <!-- Creazione sezione 2 -->
+      <section id="sec-2">
+
+      </section>
+
+      <!-- Creazione sezione 3 -->
+      <section id="sec-3">
+
+      </section>
+
+      <!-- Creazione sezione 4 -->
+      <section id="sec-4">
+        
+      </section>
+
+      <!-- Creazione sezione 5 -->
+      <section id="sec-5">
+
+      </section>
+
+      <!-- Creazione sezione 6 -->
+      <section id="sec-6">
+
+      </section>
+
+    </main>
+    <!-- Fine Main -->
+
+    <!-- Inizio Footer -->
+    <footer>
+
+      <div>
+
+      </div>
+
+      <!-- sezione link e social -->
+      <div>
+
+      </div>
+
+    </footer>
+    <!-- Fine Footer -->
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputsForm from './components/InputsForm.vue'
+import NavbarComp from './components/NavbarComp.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavbarComp,
+    InputsForm
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './styles/general.scss'
+
+
 </style>
